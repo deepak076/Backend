a78df7fda489e8da53ca7c76191f140af51b1f03
deepak076 <deepak19csu076@ncuindia.edu>
2023-10-02 16:46:47 +0530

    second commit

--- 00-starting-setup/routes/admin.js
+++ 00-starting-setup/routes/admin.js
@@ -3,7 +3,6 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
-const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
@@ -11,11 +10,15 @@ const router = express.Router();
 router.get('/add-product', adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', shopController.getProducts);
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProducts); 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
