99ea9dc8474f6590433f29aa51b1937d8f39b1a6
deepak076 <deepak19csu076@ncuindia.edu>
2023-10-08 15:54:55 +0530

    5th commit

--- 00-starting-setup/util/database.js
+++ 00-starting-setup/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'dj25082001', {
     dialect: 'mysql',
     host: 'localhost'
 });
